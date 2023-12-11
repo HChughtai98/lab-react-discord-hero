b86b3250c8e3387f002715a98b956824bc239549
Haris Chughtai <xharisahmed98@gmail.com>
2023-12-11 17:36:14 +0000

    finishing

--- discord-hero-haris/src/App.jsx
+++ discord-hero-haris/src/App.jsx
@@ -10,6 +10,29 @@ function App() {
         <img src={discordLogo} alt="DiscordLogo" />
         <img src={menuLogo}></img>
       </div>
+      <div className="textContainer">
+        <div className="mainText">
+          <h1>IMAGINE A PLACE...</h1>
+        </div>
+        <div className="subText">
+          <p>
+            ...where you can belong to a school club, a gaming group or a
+            worldwide art community. Where just you and a handful of friends can
+            spend time together. A place that makes it easy to talk every day
+            and hang out more often.
+          </p>
+        </div>
+      </div>
+      <div className="buttonContainer">
+        <div className="buttonDiv">
+          <button className="downloadBtn">Download for Mac</button>
+          <button className="openDiscordBtn">
+            Open Discord in your browser
+          </button>
+        </div>
+        <div className="spaceHolder"></div>
+        <img src={splashArt} alt="splashArt" />;
+      </div>
     </div>
   );
 }
